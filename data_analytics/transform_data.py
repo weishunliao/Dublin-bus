f78de10242d3cbb142b9a326fecac63c502a386e
fionnuala <fionnuala.wall@ucdconnect.ie>
2019-07-01 17:02:00 +0100

    Updated notebook to import functions from convert_timestamp.py

--- data_analytics/transform_data.py
+++ data_analytics/transform_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy
+import numpy as np
 
 def transform_data(df):
     """Function that takes a dataframe as input and returns a new dataframe with the required format."""
@@ -62,7 +62,7 @@ def add_missing_rows_start(count, prog, row, transformed):
         transformed[count] = missing_row(row, i)
         count += 1
     transformed[count] = [row['dayofservice'], row['tripid'], row['lineid'], row['routeid'], row['direction'], \
-                                    prog-1, numpy.nan, numpy.nan, prog, row['stoppointid'], row['actualtime_arr_stop']]
+                                    prog-1, np.nan, np.nan, prog, row['stoppointid'], row['actualtime_arr_stop']]
     count += 1
     return count, transformed
 
@@ -72,14 +72,14 @@ def add_missing_rows_mid(count, current_prog, last_prog, row, transformed):
         transformed[count] = missing_row(row, i)
         count += 1
     transformed[count] = [row['dayofservice'], row['tripid'], row['lineid'], row['routeid'], row['direction'], \
-                    current_prog-1, numpy.nan, numpy.nan, current_prog, row['stoppointid'], row['actualtime_arr_stop']]
+                    current_prog-1, np.nan, np.nan, current_prog, row['stoppointid'], row['actualtime_arr_stop']]
     count += 1
     return count, transformed
 
 def missing_row(row, i):
     """Function that creates the format for a missing row."""
     return [row['dayofservice'], row['tripid'], row['lineid'], row['routeid'], row['direction'], \
-                                            i+1, numpy.nan, numpy.nan, i+2, numpy.nan, numpy.nan]
+                                            i+1, np.nan, np.nan, i+2, np.nan, np.nan]
 
 def save_row(row, current_prog):
     """Function that creates the format for a row that should be saved."""
@@ -88,7 +88,7 @@ def save_row(row, current_prog):
 
 def complete_missing(count, prog, last_row, transformed):
     """Function that completes a row with missing data."""
-    transformed[count] = last_row + [prog + 1, numpy.nan, numpy.nan]
+    transformed[count] = last_row + [prog + 1, np.nan, np.nan]
     count += 1
     return count, transformed
 
