a86787cccd21f09b0abc49c6f6eb63f5aa5fbc21
WEISHUNLIAO <weishun.liao@gmail.com>
2019-07-26 19:35:44 +0100

    typo

--- PROJECT_DIR/django_project/dublin_bus/static/js/google_maps.js
+++ PROJECT_DIR/django_project/dublin_bus/static/js/google_maps.js
@@ -215,7 +215,7 @@ const markerListener = (stopID) => {
 const change_btn = () => {
     document.getElementById("stops__toolbar__back-btn").innerText = "";
     document.getElementById("stops__show-on-map-btn__name").style.display = 'none';
-    $("#stops__toolbar__back-btn").append("<ion-icon name='md-close' size=\"medium\"></ion-icon>");
+    $("#stops__toolbar__back-btn").append("<ion-icon name='md-close' size='medium'></ion-icon>");
     document.getElementById("stops__toolbar__back-btn").removeEventListener('click', detail);
     document.getElementById("stops__toolbar__back-btn").addEventListener('click', close_btn);
 };
