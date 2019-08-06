50abbe010748ec550161a71d493a17d148205bf9
fionnuala <fionnuala.wall@ucdconnect.ie>
2019-08-06 17:20:22 +0100

    Added unit tests for Feb

--- PROJECT_DIR/django_project/dublin_bus/tests.py
+++ PROJECT_DIR/django_project/dublin_bus/tests.py
@@ -55,6 +55,14 @@ class TestLoadModel(TestCase):
         except Exception:
             self.fail("load_model() raised an exception unexpectedly!")
 
+    def test_load_model_success_feb(self):
+        """Test to ensure that a model is loaded correctly."""
+        try:
+            month = 2
+            functions.load_model(month)
+        except Exception:
+            self.fail("load_model() raised an exception unexpectedly!")
+
 
 class TestCreateHourFeatureRef(TestCase):
     """Test cases for the create_hour_feature_ref function."""
@@ -146,6 +154,16 @@ class TestCreateSegmentRef(TestCase):
     def test_create_segment_ref_std_jan(self):
         """Test for the ouput of the create_segment_ref function."""
         segment_ref = functions.create_segment_ref(1)
+        self.assertEqual(segment_ref["1279_1282"]["std"], 37.0)
+
+    def test_create_segment_ref_mean_feb(self):
+        """Test for the ouput of the create_segment_ref function."""
+        segment_ref = functions.create_segment_ref(2)
+        self.assertEqual(segment_ref["1279_1282"]["mean"], 141.0)
+
+    def test_create_segment_ref_std_feb(self):
+        """Test for the ouput of the create_segment_ref function."""
+        segment_ref = functions.create_segment_ref(2)
         self.assertEqual(segment_ref["1279_1282"]["std"], 37.0)
 
 class TestCreateSegmentRefGtfs(TestCase):
@@ -266,6 +284,23 @@ class TestRoutePrediction(TestCase):
         school_hol = 0
         self.assertEqual(functions.route_prediction(stops, actualtime_arr_stop_first, hour, peak, \
                school_hol, weekday, rain, temp, month), 2802)
+
+    def test_route_prediction_feb(self):
+        """Test for the ouput of the route_prediction function for the 15A going in the Limekiln direction."""
+        stops = [395, 396, 397, 398, 399, 400, 7581, 1283, 7579, 1285, 1016, 1017, 1018, 1019, 1020, 1076, 1077, 1078,
+                 1079, 1080, \
+                 1081, 1082, 1083, 1085, 1086, 1087, 1088, 1089, 1090, 1091, 1092, 1093, 1094, 1095, 1096, 1101, 1102,
+                 1103, 1104]
+        rain = 0.1
+        temp = 15
+        actualtime_arr_stop_first = 32400  # 9:00
+        hour = 9
+        peak = 1
+        month = 2 # february
+        weekday = 1
+        school_hol = 0
+        self.assertEqual(functions.route_prediction(stops, actualtime_arr_stop_first, hour, peak, \
+               school_hol, weekday, rain, temp, month), 2684)
 
 class TesParseWeatherForecast(TestCase):
     """Test cases for the parse_weather_forecast function."""
