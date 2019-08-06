09427766a0691a7eb625c99f4cd0fcafec1d0235
WEISHUNLIAO <weishun.liao@gmail.com>
2019-08-06 16:28:21 +0100

    change database setting

--- PROJECT_DIR/django_project/django_project/settings.py
+++ PROJECT_DIR/django_project/django_project/settings.py
@@ -90,11 +90,11 @@ DATABASES = {
         'PORT': '3306',
 
         # PRODUCTION
-        # 'HOST': 'localhost',
+        'HOST': 'localhost',
         # 'HOST': '127.0.0.1'
 
         # UCD - 137.43.49.50 is for development
-        'HOST': '137.43.49.50'
+        # 'HOST': '137.43.49.50'
     }
 
 }
